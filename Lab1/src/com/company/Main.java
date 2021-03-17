1f38a39facb0748d15835040aac5ebb69f6f0792
Иван Васильев <vanya.vasil21@mail.ru>
2021-03-18 01:37:28 +0300

    lab1 +

--- Lab1/src/com/company/Main.java
+++ Lab1/src/com/company/Main.java
@@ -3,35 +3,46 @@ package com.company;
 import java.util.Calendar;
 import java.util.Date;
 import java.util.GregorianCalendar;
-
+/*
+ Задание
+•	Напишите программу, выводящую в консоль календарь на текущий год
+•	Дополнительно: первый день недели должен указываться через параметр командной строки
+  */
 public class Main {
 
     public static void main(String[] args) {
-    /*
-    Задание
-•	Напишите программу, выводящую в консоль календарь на текущий год
-•	Дополнительно: первый день недели должен указываться через параметр командной строки
-     */
-        GregorianCalendar calendar = new GregorianCalendar();
-        calendar.getInstance();
-        calendar.getCalendarType();
-        for(int i = 0; i<12;i++)
+        GregorianCalendar еxperimentalCalendar = new GregorianCalendar(); //Определеяем текущий год
+        int todayYear = еxperimentalCalendar.get(Calendar.YEAR);
+        String bufOtstup;
+        String[] otstup = {"","\t","\t\t","\t\t\t","\t\t\t\t","\t\t\t\t\t","\t\t\t\t\t\t",""};
+        String[] weekName = {"Пн","Вт","Ср","Чт","Пт","Сб","Вс"};
+        String[] monthName = {"Январь","Февраль","Март","Апрель","Май","Июнь","Июль","Август","Сентябрь","Октябрь","Ноябрь","Декабрь"};
+        int[] dayInMonth = {31,28,31,30,31,30,31,31,30,31,30,31};
+         if( еxperimentalCalendar.isLeapYear(todayYear))
         {
-            System.out.println(calendar.MONTH+i);
+            dayInMonth[1] = 29;
         }
-
-
-
-
-        System.out.println("SUNDAY\tMONDAY\tTUESDAY\tWEDNESDAY\tTHURSDAY\tFRIDAY\tSATURDAY");
-        System.out.println("Месяц: " + calendar.get(Calendar.DAY_OF_MONTH));
-        System.out.println("Месяц: " + calendar.get(Calendar.DAY_OF_WEEK));
-        System.out.println("Порядковый номер недели в месяце: " + calendar.get(Calendar.WEEK_OF_MONTH));//порядковый номер недели в месяце
-
-        System.out.println("Число: " + calendar.get(Calendar.DAY_OF_MONTH));
-
-
-
+        for(int m = 0; m < 12;m++)
+        {
+            System.out.println("\n"+monthName[m]);
+            System.out.print(weekName[0]+'\t'+weekName[1]+'\t'+weekName[2]+'\t'+weekName[3]+'\t'+weekName[4]+'\t'+weekName[5]+'\t'+weekName[6]);
+            System.out.println();
+            еxperimentalCalendar.set(todayYear,m,0);
+            if(еxperimentalCalendar.get(Calendar.DAY_OF_WEEK)!=0)
+            {
+                bufOtstup=otstup[еxperimentalCalendar.get(Calendar.DAY_OF_WEEK)-1];
+            }else bufOtstup=otstup[0];
+                for(int d = 0; d < dayInMonth[m];d++)
+                {
+                   еxperimentalCalendar.set(todayYear,m,d);
+                   System.out.print(bufOtstup + (d+1) + "\t");
+                    bufOtstup=otstup[0];
+                   if(еxperimentalCalendar.get(Calendar.DAY_OF_WEEK)==7)
+                   {
+                       System.out.println();
+                   }
+                }
+        }
 
     }
 }
