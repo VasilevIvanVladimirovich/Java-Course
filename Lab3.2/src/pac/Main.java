1bd23bac4365c4535bbda7fd6206692f053a1703
Иван Васильев <vanya.vasil21@mail.ru>
2021-04-03 22:09:09 +0300

    к

--- Lab3.2/src/pac/Main.java
+++ Lab3.2/src/pac/Main.java
@@ -1,13 +1,11 @@
 package pac;
 import ru.novsu.povt.viv.graphics.*;
-
 public class Main {
-
     public static void main(String[] args) {
         Rectangle rectangle1 = new Rectangle(1,1,5,6,"Красный");
         System.out.println(rectangle1.Graph());
-        Circle cirlce1 = new Circle(2,3,10,"Розовый");
-        System.out.println(cirlce1.Graph());
+        Circle circle1 = new Circle(2,3,10,"Розовый");
+        System.out.println(circle1.Graph());
         Ellipse ellipse1 = new Ellipse(4,5,5,7,"Зелёный");
         System.out.println(ellipse1.Graph());
         Background background1 = new Background("Серый","Сетка");
